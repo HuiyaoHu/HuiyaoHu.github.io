cdfe85450e008a395e7ca3ccb52ce3c8e497abc9
HuiyaoHu <huiyaohu@gmail.com>
2021-08-30 11:14:42 +0800

    clean up

--- threejs-express-template-host/facade-generator/routes/report.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET report page. */
-router.get('/', function(req, res, next) {
-  res.render('report', { title: 'Report' });
-});
-
-module.exports = router;
